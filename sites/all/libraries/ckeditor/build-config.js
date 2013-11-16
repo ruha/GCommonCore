778484197341eef0e2462e9b5057ffa77aeacc14
Ruthie Hassan <rnhassan@gmail.com>
2013-11-16 11:19:47 -0500

    Add Drupal Site

---
+++ sites/all/libraries/ckeditor/build-config.js
@@ -0,0 +1,69 @@
+﻿
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time at http://ckeditor.com/builder to build CKEditor again.
+ * 
+ * NOTE: 
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono',
+	preset: 'basic',
+	ignore: [
+		'dev',
+		'.gitignore',
+		'.gitattributes',
+		'README.md',
+		'.mailmap'
+	],
+	plugins : {
+		'basicstyles' : 1,
+		'clipboard' : 1,
+		'toolbar' : 1,
+		'entities' : 1,
+		'floatingspace' : 1,
+		'indentlist' : 1,
+		'link' : 1,
+		'list' : 1,
+		'undo' : 1,
+		'dialog' : 1,
+		'dialogui' : 1,
+		'button' : 1,
+		'indent' : 1,
+		'fakeobjects' : 1,
+		'xml' : 1,
+		'ajax' : 1,
+		'panelbutton' : 1,
+		'panel' : 1,
+		'floatpanel' : 1,
+		'colorbutton' : 1,
+		'colordialog' : 1,
+		'devtools' : 1,
+		'resize' : 1,
+		'popup' : 1,
+		'filebrowser' : 1,
+		'find' : 1,
+		'listblock' : 1,
+		'richcombo' : 1,
+		'font' : 1,
+		'horizontalrule' : 1,
+		'image' : 1,
+		'imagebrowser' : 1,
+		'menu' : 1,
+		'contextmenu' : 1,
+		'liststyle' : 1,
+		'pastetext' : 1,
+		'pastefromword' : 1
+	},
+	languages : {
+		'en' : 1,
+		'es' : 1
+	}
+};
